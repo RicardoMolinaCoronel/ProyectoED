899ca6953d733dd92d33b5f0bdb4b505298c9329
DvMarc <romdmarc@espol.edu.ec>
2021-11-29 20:11:52 -0500

    agregadas clases coordenada y specializedButton

---
+++ src/Modelo/Coordenada.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author rdavi
+ */
+public class Coordenada {
+    private int fila;
+    private int columna;
+    public boolean ocurrencia;
+    
+    public Coordenada(int fila, int columna) {
+        this.fila = fila;
+        this.columna = columna;
+        ocurrencia = false;
+    }
+
+    public Coordenada(int fila, int columna, boolean ocurrencia) {
+        this.fila = fila;
+        this.columna = columna;
+        this.ocurrencia = ocurrencia;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public boolean isOcurrencia() {
+        return ocurrencia;
+    }
+
+    @Override
+    public String toString() {
+        return fila + " " + columna;
+    }
+}
